99f4fb4f73b1a196541cf397d470eeafa46207c5
permania <0onesixed@gmail.com>
2026-03-04 10:32:30 -0800

    init: create project structure

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "My project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    crane.url = "github:ipetkov/crane";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    crane,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        craneLib = crane.mkLib pkgs;
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cargo
+            clippy
+            rust-analyzer
+          ];
+        };
+
+        packages.default = craneLib.buildPackage {
+          src = ./.;
+          buildInputs = with pkgs; [];
+        };
+      }
+    );
+}
---
+++ gravity/src/main.rs
@@ -0,0 +1,5 @@
+use gravlib::add;
+
+fn main() {
+    println!("{}", add(1, 2));
+}
